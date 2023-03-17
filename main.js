e0f9201ad81381384f114015c7077c5df30d79bf
ninjadev <ninjadev333@gmail.com>
2023-03-16 21:06:05 -0500

    navbar

---
+++ main.js
@@ -0,0 +1,13 @@
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  // primaryNav.classList.toggle("opened");
+  /* toggle aria-expanded based on data-visible attribute on the navToggle */
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+});
